fad84a32d91139156a168ae54d3d066734c45030
atsushi yano <a.yano.578@ms.saitama-u.ac.jp>
2019-12-14 17:21:57 +0900

    first commit

---
+++ hackFuture-NGshare/hackFuture-NGshare/CameraViewController.swift
@@ -0,0 +1,30 @@
+//
+//  CameraViewController.swift
+//  hackFuture-NGshare
+//
+//  Created by ATSUSHI YANO on 2019/12/14.
+//  Copyright © 2019 ATSUSHI YANO. All rights reserved.
+//
+
+import UIKit
+
+class CameraViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
