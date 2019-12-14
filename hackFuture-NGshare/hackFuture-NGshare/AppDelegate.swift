28b40a7624d05cf68056e6a8bf4efcd7ead0a89e
Yuto Terasawa <y.terasawa.498@ms.saitama-u.ac.jp>
2019-12-14 23:09:53 +0900

    APIのgetを実装（但し一列のみ）

--- hackFuture-NGshare/hackFuture-NGshare/AppDelegate.swift
+++ hackFuture-NGshare/hackFuture-NGshare/AppDelegate.swift
@@ -12,8 +12,6 @@ import UIKit
 class AppDelegate: UIResponder, UIApplicationDelegate {
 
     var window: UIWindow?
-    var test: String
-    var test1:String
 
     func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?) -> Bool {
         // Override point for customization after application launch.
--- hackFuture-NGshare/hackFuture-NGshare/CameraViewController.swift
+++ hackFuture-NGshare/hackFuture-NGshare/CameraViewController.swift
@@ -49,7 +49,6 @@ class CameraViewController: UIViewController {
      
      override func viewDidLoad() {
          super.viewDidLoad()
-         
          // Do any additional setup after loading the view.
      }
      
