52c9460b2c11440dfb2c6d39edf7f634c3b0b20c
yunicoder <t.yabe.240@ms.saitama-u.ac.jp>
2019-12-14 16:39:16 +0900

    Initial Commit

---
+++ NGShare/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  NGShare
+//
+//  Created by Takuma Yabe on 2019/12/14.
+//  Copyright © 2019 Takuma Yabe. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
