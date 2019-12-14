98fabe825144b7f46274b66fb963ee291dec2bf4
yunicoder <t.yabe.240@ms.saitama-u.ac.jp>
2019-12-14 18:28:15 +0900

    カメラのプレビューレイヤーの追加。カメラ画面が見えるようになる。

---
+++ hackFuture-NGshare/hackFuture-NGshare/ExtentionViewController.swift
@@ -0,0 +1,31 @@
+//
+//  ExtentionViewController.swift
+//  hackFuture-NGshare
+//
+//  Created by Takuma Yabe on 2019/12/14.
+//  Copyright © 2019 ATSUSHI YANO. All rights reserved.
+//
+
+//import Photos
+import PhotosUI
+
+// デリゲート部分を拡張する
+extension CameraViewController: AVCapturePhotoCaptureDelegate {
+    
+    // 映像をキャプチャする
+    func photoOutput(_ output: AVCapturePhotoOutput,
+                     didFinishProcessingPhoto photo: AVCapturePhoto, error: Error?) {
+        
+        // Dataを取り出す
+        guard let photoData = photo.fileDataRepresentation() else {
+            return
+        }
+        // Dataから写真イメージを作る
+        if let stillImage = UIImage(data: photoData) {
+            // アルバムに追加する
+            //UIImageWriteToSavedPhotosAlbum(stillImage, self, #selector(image(_:didFinishSavingWithError:contextInfo:)), nil)
+            
+        }
+    }
+    
+}
