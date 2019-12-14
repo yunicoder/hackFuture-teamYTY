c6e1dfbb8c59e3dd1f4c88192d3ee322381dcd21
yunicoder <t.yabe.240@ms.saitama-u.ac.jp>
2019-12-14 22:14:59 +0900

    test2

--- hackFuture-NGshare/hackFuture-NGshare/AppDelegate.swift
+++ hackFuture-NGshare/hackFuture-NGshare/AppDelegate.swift
@@ -13,6 +13,7 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
 
     var window: UIWindow?
     var test: String
+    var test1:String
 
     func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?) -> Bool {
         // Override point for customization after application launch.
