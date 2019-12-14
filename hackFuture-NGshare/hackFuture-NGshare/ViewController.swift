5acf388c2320ed0f9ea46347f91e17554c5379e4
atsushi yano <a.yano.578@ms.saitama-u.ac.jp>
2019-12-14 16:52:32 +0900

    first commit

---
+++ hackFuture-NGshare/hackFuture-NGshare/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  hackFuture-NGshare
+//
+//  Created by ATSUSHI YANO on 2019/12/14.
+//  Copyright © 2019 ATSUSHI YANO. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
