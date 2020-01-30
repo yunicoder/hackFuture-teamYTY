197d28b1505b1983f633734eda67b07f526a2209
Yuto Terasawa <y.terasawa.498@ms.saitama-u.ac.jp>
2020-01-30 23:58:10 +0900

    コメント入力時のキーボードに完了ボタン追加

--- hackFuture-NGshare/hackFuture-NGshare/GoodsInfoViewController.swift
+++ hackFuture-NGshare/hackFuture-NGshare/GoodsInfoViewController.swift
@@ -85,6 +85,8 @@ class GoodsInfoViewController: UIViewController, UITextFieldDelegate, UITextView
         goodsPriceTextField.inputAccessoryView = custombar
         goodsPriceTextField.keyboardType = UIKeyboardType.numberPad
         goodsPriceTextField.delegate = self
+        goodsCommentTextField.inputAccessoryView = custombar
+        goodsCommentTextField.keyboardType = .default
         
         //時間はドラムロール
         goodsTimeTextField.inputView = datePicker
@@ -276,6 +278,9 @@ class GoodsInfoViewController: UIViewController, UITextFieldDelegate, UITextView
             goodsPriceTextField.resignFirstResponder()
             registerGoods.price = goodsPriceTextField.text!
         }
+        if(goodsCommentTextField.isFirstResponder){
+            goodsCommentTextField.resignFirstResponder()
+        }
     }
     
     // "購入画面"ボタンが押された時の処理
@@ -329,48 +334,4 @@ class GoodsInfoViewController: UIViewController, UITextFieldDelegate, UITextView
     
 }
 
-@IBDesignable class InspectableTextView: UITextView {
 
-    // MARK: - プロパティ
-    /// プレースホルダーに表示する文字列（ローカライズ付き）
-    @IBInspectable var localizedString: String = "" {
-        didSet {
-            guard !localizedString.isEmpty else { return }
-            // Localizable.stringsを参照する
-            placeholderLabel.text = NSLocalizedString(localizedString, comment: "(例)使用感はあまりないです")
-            placeholderLabel.sizeToFit()  // 省略不可
-        }
-    }
-
-    /// プレースホルダー用ラベルを作成
-    private lazy var placeholderLabel = UILabel(frame: CGRect(x: 6, y: 6, width: 0, height: 0))
-
-    // MARK: - Viewライフサイクルメソッド
-    /// ロード後に呼ばれる
-    override func awakeFromNib() {
-        super.awakeFromNib()
-        delegate = self
-        configurePlaceholder()
-        togglePlaceholder()
-    }
-
-    /// プレースホルダーを設定する
-    private func configurePlaceholder() {
-        placeholderLabel.textColor = UIColor.gray
-        addSubview(placeholderLabel)
-    }
-
-    /// プレースホルダーの表示・非表示切り替え
-    private func togglePlaceholder() {
-        // テキスト未入力の場合のみプレースホルダーを表示する
-        placeholderLabel.isHidden = text.isEmpty ? false : true
-    }
-}
-
-// MARK: -  UITextView Delegate
-extension InspectableTextView: UITextViewDelegate {
-    /// テキストが書き換えられるたびに呼ばれる ※privateにはできない
-    func textViewDidChange(_ textView: UITextView) {
-        togglePlaceholder()
-    }
-}
